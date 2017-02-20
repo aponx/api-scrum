cc3edf8d99a652358b412d5e4ccb21be3d390daf
Aprilia Ramadhayanti <apinx01@gmail.com>
2017-02-21 00:16:36 +0700

    coba ini

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=scrum',
+	'connectionString' => 'mysql:host=127.0.0.1;dbname=u389078254_scrum',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => 'kiryono',
+	'username' => 'u389078254_root',
+	'password' => '024Aponx1991',
 	'charset' => 'utf8',
 	
 );
--- protected/controllers/JapriController.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class JapriController extends CBJsonController
-	{
-		
-	}
-?>
